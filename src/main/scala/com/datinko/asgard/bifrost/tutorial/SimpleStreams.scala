3c6a96101cee24eccec8907593a0239323ed7fc9
Neil Dunlop <neil.dunlop@williamhill.com>
2015-12-30 01:09:40 +0000

    Tutorial WIP

--- src/main/scala/com/datinko/asgard/bifrost/tutorial/SimpleStreams.scala
+++ src/main/scala/com/datinko/asgard/bifrost/tutorial/SimpleStreams.scala
@@ -1,7 +1,12 @@
 package com.datinko.asgard.bifrost.tutorial
 
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.actor.Props
+import akka.stream.{ClosedShape, ActorMaterializer}
+import akka.stream.scaladsl._
+import com.datinko.asgard.bifrost.ThrottledProducer
+import com.datinko.asgard.bifrost.actors.DelayingActor
+import io.scalac.amqp.Message
+import scala.concurrent.duration._
 
 /**
  * An examples of the simplest possible akka stream.
@@ -16,5 +21,23 @@ object SimpleStreams {
       .map(println(_))
       .to(Sink.ignore)
       .run()
+  }
+
+  def throttledProducerToConsole(implicit materializer: ActorMaterializer) = {
+
+    val theGraph = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[Unit] =>
+
+      import GraphDSL.Implicits._
+
+      val source = builder.add(ThrottledProducer.produceThrottled(materializer, 1 second, 20 milliseconds, 20000, "fastProducer"))
+      val printFlow = builder.add(Flow[(Message)].map{println(_)})
+
+      val sink = builder.add(Sink.ignore)
+
+      source ~> printFlow ~> sink
+
+      ClosedShape
+    })
+    theGraph
   }
 }
